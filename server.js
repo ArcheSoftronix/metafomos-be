f3a1bc5974f21f37becb143d272372adb664af8a
super-dev008 <super008008@outlook.com>
2022-02-12 06:31:49 +0200

    Add cors

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const connectDB = require('./config/db');
 
 const bodyParser = require('body-parser');
@@ -7,6 +8,8 @@ const app = express();
 
 //Connect Database
 connectDB();
+
+app.use(cors());
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
